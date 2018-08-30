8cae9b829038ad76fc7769eecc171373bde5d976
Rhys <rhys@rhyswynne.co.uk>
2018-08-30 08:47:14 +0100

    Included the CSS within the plugin so it's self contained

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -8,10 +8,8 @@
 function faw_enqueue_datepicker() {
 
 	wp_enqueue_script( 'faw-datepicker', FAW_PLUGIN_URL . '/js/datepicker-script.js', array( 'jquery-ui-datepicker', 'jquery' ), FAW_PLUGIN_VERSION );
-	//wp_enqueue_style( 'jquery-ui', FAW_PLUGIN_URL . '/css/jquery-ui.min.css' );
-	//wp_enqueue_style( 'jquery-ui-theme', FAW_PLUGIN_URL . '/css/jquery-ui.theme.min.css' );
-
-	wp_register_style('jquery-ui', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css');
-	wp_enqueue_style( 'jquery-ui' ); 
+	wp_enqueue_style( 'jquery-ui', FAW_PLUGIN_URL . '/css/jquery-ui.min.css' );
+	wp_enqueue_style( 'jquery-ui-structure', FAW_PLUGIN_URL . '/css/jquery-ui.structure.min.css' );
+	wp_enqueue_style( 'jquery-ui-theme', FAW_PLUGIN_URL . '/css/jquery-ui.min.css' );
 
 } add_action( 'admin_enqueue_scripts', 'faw_enqueue_datepicker' );
