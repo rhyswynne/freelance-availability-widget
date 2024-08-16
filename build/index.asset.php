1b914bdba65af47130cf2e3efbc18d1573864830
Rhys Wynne <rhys@dwinrhys.com>
2024-08-16 09:49:57 +0100

    Bumped version number & a few package fixes

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0d2dcf5fe1ae1b90009a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b0bf6297c339d913d0d4');
