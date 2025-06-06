654fed893afa850a0e69af881ccd33d30736291d
Rhys Wynne <rhys@dwinrhys.com>
2025-06-06 11:10:59 +0100

    Fixe another bunch of security issues

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b0bf6297c339d913d0d4');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '57f63365c5eed05891bc');
